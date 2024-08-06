3f32dd56081ba0bd1d4eb09d2da1b7519bb2c2c2
R-976 <141494436+R-976@users.noreply.github.com>
2024-08-07 00:41:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "13335263"))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-STRING_SESSION = getenv("STRING_SESSION", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5392070730").split()))
+API_ID = int(getenv("API_ID", "13146123"))
+API_HASH = getenv("API_HASH", "53e10a257baaddea9c413a0c4249e731")
+BOT_TOKEN = getenv("BOT_TOKEN", "7239889663:AAHGtiWGzzW2jmBilVr7XCJILQgF7LJwWJo")
+OWNER_ID = int(getenv("OWNER_ID", "5378060239"))
+STRING_SESSION = getenv("STRING_SESSION", "BQEiZi4AuFRdcuUrR_eBV42Mv5h3u61GyFWOPqXIf1bEaW-AyO15pxTtYeDcW3JqJKH92uiIiTr4kRnd6_is_sdZRcUughaQQFhx-mUbbOF5RB8crrXvhWyU65GWhfQNjw9xtoyKLMWdq0dau5-bBq1-zTjTsheSrkHKKwvbobPHWMC8sZu6WQg39QQIT0E4F6LOkxjMndz2VwY2d3RNBQoFspA_uplLznDT5Jx77_UX0YHSvAeEQa3Qa_1dJZX5suQj20nnybYbc2oTNXjRZpKtuA7I5t_LahVhHvgSS5JsGGgZ7WoRQA6mc4y2ncJ8K7Mhzb26wL7DycLd9dz0zXslKLkDbQAAAAFAjq_PAA")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5378060239").split()))
 ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/a62b9c7d9848afde0569e.jpg")
 REPO_URL = getenv("REPO_URL", "https://github.com/RRomeo-RJ/Romeo-UserBot")
 BRANCH = getenv("BRANCH", "main")
